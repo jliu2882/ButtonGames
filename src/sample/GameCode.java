ff0528895dccd29cafffeee2ff7beda45d8a4d88
BT_1N3_32 <jliu2882bths.edu>
2019-01-07 11:43:22 -0500

    ????????????????????????????????????????????????????????????????????????????????????????

--- src/sample/GameCode.java
+++ src/sample/GameCode.java
@@ -4,6 +4,10 @@ import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
@@ -15,6 +19,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -24,8 +30,9 @@ public class GameCode extends Application {
     private Button button3;
     private Button button4;
     private TextField uname;
-    private int x;
     private boolean gameOn;
+    private int x;
+    private List<Integer> sequence = new ArrayList<>();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -46,6 +53,31 @@ public class GameCode extends Application {
         button2 = new Button("2");
         button3 = new Button("3");
         button4 = new Button("4");
+        button1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                button1.setStyle("-fx-background-color: #40ff00;");
+                waits();
+            }
+        });
+        button2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                button2.setStyle("-fx-background-color: #40ff00;");
+                waits();
+            }
+        });
+        button3.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                button3.setStyle("-fx-background-color: #40ff00;");
+                waits();
+            }
+        });
+        button4.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                button4.setStyle("-fx-background-color: #40ff00;");
+                waits();
+            }
+        });
+
         button1.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
         button2.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
         button3.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
@@ -88,25 +120,91 @@ public class GameCode extends Application {
                 System.out.println(uname.getText());
 
                 gameOn = true;
-                while(gameOn){
-                    blink(x);
-                    x++;
-                }
+                x=1;
+                blink();
             }
         });
         
     }
-
-    public void blink(int count){
-        button1.setStyle("-fx-background-color: #40ff00");
+    public void blink(){
+        int round=2;
         Timeline timeline = new Timeline(new KeyFrame(
                 Duration.millis(200),
-                ae -> System.out.println("ping")));
-        timeline.setCycleCount(2);
-        timeline.play();
-        button1.setStyle("-fx-background-color: #33cc00");
+                ae -> sequence()));
+            timeline.setCycleCount(x);
+            timeline.play();
+    }
+    public void isUserRight(){
 
-        gameOn = false;
+    }
+    public void sequence(){
+        int rand = (int)(Math.random()*4);
+        sequence.add(rand);
+
+        if(rand==0){
+            button1.setStyle("-fx-background-color: #40ff00;");
+        }
+        if(rand==1){
+            button2.setStyle("-fx-background-color: #40ff00;");
+        }
+        if(rand==2){
+            button3.setStyle("-fx-background-color: #40ff00;");
+        }
+        if(rand==3){
+            button4.setStyle("-fx-background-color: #40ff00;");
+        }
+        waits();
+
+        for(int i =0;i<sequence.size();i++) {
+            System.out.println(sequence.get(i));
+        }
+        System.out.println(" ");
+
+
+    }
+    //?????????????????????????
+    public void waits(){
+        //?????????????????????????
+        Task<Void> sleeper = new Task<Void>() {
+            //?????????????????????????
+            @Override
+            //?????????????????????????
+            protected Void call() throws Exception {
+                //?????????????????????????
+                try {
+                    //?????????????????????????
+                    Thread.sleep(300);
+                    //?????????????????????????
+                } catch (InterruptedException e) {
+                    //?????????????????????????
+                }
+                //?????????????????????????
+                return null;
+                //?????????????????????????
+            }
+            //?????????????????????????
+        };
+        //?????????????????????????
+        sleeper.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+            //?????????????????????????
+            @Override
+            //?????????????????????????
+            public void handle(WorkerStateEvent event) {
+                //?????????????????????????
+                button1.setStyle("-fx-background-color: #33cc00;");
+                //?????????????????????????
+                button2.setStyle("-fx-background-color: #33cc00;");
+                //?????????????????????????
+                button3.setStyle("-fx-background-color: #33cc00;");
+                //?????????????????????????
+                button4.setStyle("-fx-background-color: #33cc00;");
+                //?????????????????????????
+            }
+            //?????????????????????????
+        });
+        //?????????????????????????
+        new Thread(sleeper).start();
+        //?????????????????????????
     }
     public static void main(String[] args) {
         launch(args);
